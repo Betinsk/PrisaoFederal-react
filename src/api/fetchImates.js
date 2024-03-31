50399fa5273d1a172eefe843b7d5ffe7e6a4da83
betinsk <gmdust@hotmail.com>
2024-03-30 23:10:50 -0300

    novo cadastro imate com api java

--- src/api/fetchImates.js
+++ src/api/fetchImates.js
@@ -1,7 +1,7 @@
 
 const fetchImates = async () => {
 
-        const resposta = await fetch('https://randomuser.me/api/?results=25')
+        const resposta = await fetch('http://localhost:8080/imates')
         const dadosJson = await resposta.json()
         
         return dadosJson.results
