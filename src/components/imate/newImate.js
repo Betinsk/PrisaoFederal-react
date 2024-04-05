6d8692c4273c1ec0ae7035416e50ecf4d560fb58
betinsk <gmdust@hotmail.com>
2024-04-04 21:59:45 -0300

    method post

--- src/components/imate/newImate.js
+++ src/components/imate/newImate.js
@@ -4,6 +4,12 @@ function NewImate() {
 
     const [jsonData, setData] = useState([]);
 
+    const [imate, setImate] = useState({
+      age: '',
+      gender: '',
+      name: ''
+    });
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -22,7 +28,41 @@ function NewImate() {
     fetchData();
   }, []);
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setImate(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:8080/imates', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(imate),
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Imate criado com sucesso:', data);
+      // Faça algo após o sucesso, como redirecionar para outra página
+    })
+    .catch(error => {
+      console.error('Ocorreu um erro ao criar o Imate:', error);
+    });
+  };
+
+
         return (
+          <>
             <div>
                   {jsonData.map((imate, index) => (
                     <div key={imate.id}>
@@ -33,6 +73,26 @@ function NewImate() {
                   ))}
             
             </div>
+
+                    <div>
+                      Criando imate
+                    <form onSubmit={handleSubmit}>
+                    Age:
+                    <input type="text" name="age" value={imate.age} onChange={handleChange} />
+
+                    Gender:
+                    <input type="text" name="gender" value={imate.gender} onChange={handleChange} />
+  
+                    Name:
+                    <input type="text" name="name" value={imate.name} onChange={handleChange} />
+
+                    <button type="submit">Submit</button>
+
+                      </form>
+
+                    </div>
+                    </>
+
         )
     }
     export default NewImate
