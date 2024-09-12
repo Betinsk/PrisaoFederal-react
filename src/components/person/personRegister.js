84d1a1edaadeed74a3b6d8310bad475b0150924c
Betinsk <gmdust@hotmail.com>
2024-09-12 16:51:51 -0300

    Ajustes no imateVisitor

--- src/components/person/personRegister.js
+++ src/components/person/personRegister.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import './person.css'
 
 function PersonRegister() {
   const [nome, setNome] = useState('');
+  const [age, setAge] = useState('')
+  const [socialSecure, setSocialSecure] = useState('')
+  const [cellFoneNumber, setcellFoneNumber] = useState('')
+
+
+  
   const [imate, setData] = useState([''])
   // Adicione mais estados conforme necessário para outros campos do formulário
-  const [selectedOption, setSelectedOption] = useState(['']);
+  const [selectedOption, setSelectedOption] = useState(1); // Esse useState 1, significa que eu estou por padrão acessando o primeiro elemento da lista
 
 
+  //Trás minha lista de presos
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -38,6 +46,10 @@ function PersonRegister() {
     // Construa o objeto com os dados da entidade a serem enviados para o backend
     const newImateVisitor = {
       name: nome,
+      age: age,
+      socialSecure: socialSecure,
+      cellFoneNumber: cellFoneNumber,
+     
       // O objeto espera o id incapsulado dentro de um objeto, caso contrario
       // da o erro JSON parse error: Cannot construct instance of `com.example.demo.domain.Imate` (although at least one Creator exists): no String-argument constructor/factory method to deserialize from String value ('5')]
       imate: {
@@ -75,36 +87,45 @@ function PersonRegister() {
   };
 
   return (
-    <>
+    <div className='container'>
     
-    <div>
-<select onChange={handleSelectChange} >
-  {/* Mapeia o array para gerar as opções */}
-  {imate.map((option, index) => (   
-    <option key={index} value={option.id} >
-        {/* Condicional esperando o array carregar,
-         enquanto isso ele exibe o nome carregando
-        */}
-        {option.name ? option.name: 'Carregando'}
-    </option>
+    <div className='listaImates'>
+    
+        <select onChange={handleSelectChange} >
+      
+          {/* Mapeia o array para gerar as opções */}
+          {imate.map((option, index) => (   
+            <option key={index} value={option.id} >
+                {/* Condicional esperando o array carregar,
+                enquanto isso ele exibe o nome carregando
+                */}
+                {option.name ? option.name: 'Carregando'}
+            </option>
 
-  ))}
-</select>
+          ))} 
+        </select>
+        <p className=''> Select the family's imate</p>
+  </div>
+
+      <div className='form'>
+        <form onSubmit={handleSubmit}>
+          <label>
+          <p> Name:</p>
+            <input type="text" value={nome} onChange={(event) => setNome(event.target.value)} />
+            <p> Age:</p> 
+                <input type="text" value={age} onChange={(event) => setAge(event.target.value)} />
+                <p>Cell fone number: </p> 
+                <input type="text" value={cellFoneNumber} onChange={(event) => setcellFoneNumber(event.target.value)} />
+                <p>Social Secure</p>
+            <input type="text" value={socialSecure} onChange={(event) => setSocialSecure(event.target.value)} />
+          </label>
+        <br/>
+        {/* Adicione mais campos do formulário conforme necessário */}
+      <button  type="submit">Create</button>
+      </form>
+      </div>
+
 </div>
-
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input type="text" value={nome} onChange={(event) => setNome(event.target.value)} />
-
-      </label>
-      <br/>
-      {/* Adicione mais campos do formulário conforme necessário */}
-      <button type="submit">Create</button>
-    </form>
-
-
-</>
   );
 }
 
