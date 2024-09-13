2f9dae6c5a6692b5837387410595c7a209a707b1
Betinsk <gmdust@hotmail.com>
2024-09-13 17:44:21 -0300

    Corrigido o bug que quando o person fosse deletado a pagina não atualizava

--- src/components/imate/newImate.js
+++ src/components/imate/newImate.js
@@ -1,16 +1,16 @@
-import { useState} from "react";
+import { useState } from "react";
 import './imate.css'
 
 function NewImate() {
 
 
-    const [imate, setImate] = useState({
-      age: '',
-      gender: '',
-      name: ''
-    });
+  const [imate, setImate] = useState({
+    age: '',
+    gender: '',
+    name: ''
+  });
 
-    
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setImate(prevState => ({
@@ -18,7 +18,7 @@ function NewImate() {
       [name]: value
     }));
   };
-  
+
 
   const handleSubmit = (e) => {
     fetch('http://localhost:8080/imates', {
@@ -28,45 +28,41 @@ function NewImate() {
       },
       body: JSON.stringify(imate),
     })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('Imate criado com sucesso:', data);
-      // Faça algo após o sucesso, como redirecionar para outra página
-    })
-    .catch(error => {
-      console.error('Ocorreu um erro ao criar o Imate:', error);
-    });
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Imate criado com sucesso:', data);
+        // Faça algo após o sucesso, como redirecionar para outra página
+      })
+      .catch(error => {
+        console.error('Ocorreu um erro ao criar o Imate:', error);
+      });
   };
 
 
-        return (
-        
-                    <div className="divCreateImate">
-                      Criando imate
-                    <form onSubmit={handleSubmit}>
-                      <div className='inputs'>
-                        Age:
-                        <input type="text" name="age" value={imate.age} onChange={handleChange} />
-
-                        Gender:
-                        <input type="text" name="gender" value={imate.gender} onChange={handleChange} />
-      
-                        Name:
-                        <input type="text" name="name" value={imate.name} onChange={handleChange} />
-
-                        Social Security:
-                        <input type="text" name="socialSecurity" value={imate.socialSecurity} onChange={handleChange} />
-
-                          <button className='button-38' type="submit">Submit</button>
-                        </div>
-                      </form>
-                      </div>
-              
-        )
-    }
-    export default NewImate
+  return (
+    <div className="container">
+      <div className="divCreateImate">
+        Criando imate
+        <form onSubmit={handleSubmit}>
+          <div className='inputs'>
+            <p> Age:</p>
+              <input type="text" name="age" value={imate.age} onChange={handleChange} />
+                 <p>Gender:</p>
+                   <input type="text" name="gender" value={imate.gender} onChange={handleChange} />
+                   <p>Name:</p>
+                 <input type="text" name="name" value={imate.name} onChange={handleChange} />
+               <p>Social Security:</p>
+              <input type="text" name="socialSecurity" value={imate.socialSecurity} onChange={handleChange} />
+            <button className='button-38' type="submit">Create</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+export default NewImate
--- src/components/person/personConsult.js
+++ src/components/person/personConsult.js
@@ -30,6 +30,26 @@ function PersonConsult() {
         fetchData();
       }, []);
 
+      
+      const handleDeleteVisitor = async (id) => {
+        try {
+            const response = await fetch(`http://localhost:8080/visitor/${id}`, {
+                method: 'DELETE'
+            });
+            console.log(response)
+
+            if (response.ok) {
+                // Atualize o estado ou faça qualquer ação necessária após a exclusão bem-sucedida
+               setData(jsonData.filter(item => item.id !== id));
+
+                console.log('Visitor excluído com sucesso!');
+            } else {
+                console.error('Falha ao excluir visitor, ele não existe ou possui pendencias');
+            }
+        } catch (error) {
+            console.error('Erro ao processar solicitação de exclusão:', error);
+        }
+    };
 
     return (
         <div className="">
@@ -61,7 +81,7 @@ function PersonConsult() {
 
                       </div>
 
-                      <DeletPerson visitorId={person.id} />
+                          <button className='button-38' onClick={() => handleDeleteVisitor(person.id)}>Delete</button >
 
                     </div>
                   ))}
