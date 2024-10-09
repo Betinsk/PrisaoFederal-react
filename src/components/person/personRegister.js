94cfd76499ad97a6b7ff9c588ee00d02494ebc80
Betinsk <gmdust@hotmail.com>
2024-10-09 18:40:00 -0300

    limpeza no cod

--- src/components/person/personRegister.js
+++ src/components/person/personRegister.js
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from 'react';
 import './person.css'
 
 function PersonRegister() {
-  
   const [imate, setData] = useState([])
   //const [selectedOption, setSelectedOption] = useState(1); // Esse useState 1, significa que eu estou por padrão acessando o primeiro elemento da lista
-
+  
     const [visitor, setVisitor] = useState({
       name: '',
       age: '',
@@ -56,26 +55,9 @@ function PersonRegister() {
     fetchData();
   }, []);
 
-   
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-    // // Construa o objeto com os dados da entidade a serem enviados para o backend
-    // const newImateVisitor = {
-    //   name: nome,
-    //   age: age,
-    //   socialSecure: socialSecure,
-    //   cellfoneNumber: cellFoneNumber,
-     
-    //   // O objeto espera o id incapsulado dentro de um objeto, caso contrario
-    //   // da o erro JSON parse error: Cannot construct instance of `com.example.demo.domain.Imate` (although at least one Creator exists): no String-argument constructor/factory method to deserialize from String value ('5')]
-    //   imates: [ {
-    //     id: selectedOption
-    //   }
-    // ]
-    //   // Adicione outros campos aqui conforme necessário
-    // };
     try {
       // Envie os dados para o backend usando uma requisição HTTP (por exemplo, usando fetch ou axios)
       const response = await fetch('http://localhost:8080/visitor', {
@@ -89,9 +71,7 @@ function PersonRegister() {
 
       if (response.ok) {
         // Limpe os campos do formulário após o envio bem-sucedido
-        
-        // Limpe outros campos do formulário conforme necessário
-
+      
         console.log('Entidade criada com sucesso!');
       } else {
         // Manipule erros de requisição aqui, se necessário
@@ -107,10 +87,7 @@ function PersonRegister() {
     <div className='container'>
     
     <div className='listaImates'>
-    
-        <select onChange={handleSelectChange}
- >
-      
+        <select onChange={handleSelectChange} >
           {/* Mapeia o array para gerar as opções */}
           {imate.map((option, index) => (   
             <option key={index} value={option.id} >
@@ -119,7 +96,6 @@ function PersonRegister() {
                 */}
                 {option.name ? option.name: 'Carregando'}
             </option>
-
           ))} 
         </select>
         <p className=''> Select the family's imate</p>
